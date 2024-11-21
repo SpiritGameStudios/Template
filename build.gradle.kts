f74ae65ed263c9b67a39cbb5480eb638605bd702
CallMeEchoCodes <romanbarrettsarpi@pm.me>
2024-11-21 15:59:48 +1100

    feat: tons of reworks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,16 @@
 plugins {
-    java
-    id("fabric-loom") version "1.7-SNAPSHOT"
+	java
+	alias(libs.plugins.fabric.loom)
+	alias(libs.plugins.minotaur)
 }
 
 class ModInfo {
-    val id = property("mod.id").toString()
-    val group = property("mod.group").toString()
-    val version = property("mod.version").toString()
-}
-
-class Dependencies {
-    val minecraft = property("deps.minecraft").toString()
-    val loader = property("deps.loader").toString()
-    val yarn = property("deps.yarn").toString()
-
-    val fabricApi = property("deps.fabricapi").toString()
+	val id = property("mod.id").toString()
+	val group = property("mod.group").toString()
+	val version = property("mod.version").toString()
 }
 
 val mod = ModInfo()
-val deps = Dependencies()
 
 version = mod.version
 group = mod.group
@@ -26,53 +18,62 @@ group = mod.group
 base.archivesName = "${mod.id}-${mod.version}"
 
 loom {
-    splitEnvironmentSourceSets()
-    accessWidenerPath = file("src/main/resources/${mod.id}.accesswidener")
+	splitEnvironmentSourceSets()
 
-    mods.create(mod.id) {
-        sourceSet(sourceSets.getByName("main"))
-        sourceSet(sourceSets.getByName("client"))
-    }
+	mods.create(mod.id) {
+		sourceSet(sourceSets.getByName("main"))
+		sourceSet(sourceSets.getByName("client"))
+	}
 }
 
 repositories {
-    mavenCentral()
-    maven("https://maven.callmeecho.dev/releases/")
+	mavenCentral()
+	maven("https://maven.spiritstudios.dev/releases/")
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:${deps.minecraft}")
-    mappings("net.fabricmc:yarn:${deps.yarn}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${deps.loader}")
+	minecraft(libs.minecraft)
+	mappings(variantOf(libs.yarn) { classifier("v2") })
+	modImplementation(libs.fabric.loader)
 
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${deps.fabricApi}")
+	modImplementation(libs.fabric.api)
 }
 
 tasks.processResources {
-    inputs.property("id", mod.id)
-    inputs.property("version", mod.version)
-    inputs.property("loader_version", deps.loader)
-    inputs.property("minecraft_version", deps.minecraft)
+	val map = mapOf(
+		"mod_id" to mod.id,
+		"mod_version" to mod.version,
+		"fabric_loader_version" to libs.versions.fabric.loader.get(),
+		"minecraft_version" to libs.versions.minecraft.get()
+	)
 
-    val map = mapOf(
-        "id" to mod.id,
-        "version" to mod.version,
-        "loader_version" to deps.loader,
-        "minecraft_version" to deps.minecraft
-    )
-
-    filesMatching("fabric.mod.json") { expand(map) }
+	inputs.properties(map)
+	filesMatching("fabric.mod.json") { expand(map) }
 }
 
 java {
-    withSourcesJar()
+	withSourcesJar()
 
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-    options.release = 21
+	options.encoding = "UTF-8"
+	options.release = 21
 }
 
+tasks.jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+
+modrinth {
+	token.set(System.getenv("MODRINTH_TOKEN"))
+	projectId.set(mod.id)
+	versionNumber.set(mod.version)
+	uploadFile.set(tasks.remapJar)
+	gameVersions.addAll(libs.versions.minecraft.get(), "1.21.1")
+	loaders.addAll("fabric", "quilt")
+	syncBodyFrom.set(rootProject.file("README.md").readText())
+	dependencies {
+		required.version("fabric-api", libs.versions.fabric.api.get())
+	}
+}
